d7a0940aed60cc26b26a5fa49fe50d49cfcdfcc6
Tavilo Breno <breno_wk2@hotmail.com>
2022-11-08 14:00:18 -0300

    Criando o model

---
+++ models/Job.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const db = require('../db/connection');
+
+const job = db.define('job',  {
+    title: {
+        type: Sequelize.STRING,
+    },
+    description: {
+        type: Sequelize.STRING,
+    },
+    salary: {
+        type: Sequelize.STRING,
+    },
+    company: {
+        type: Sequelize.STRING,
+    },
+    new_job: {
+        type: Sequelize.INTEGER,
+    },
+});
+
+module.exports = job
